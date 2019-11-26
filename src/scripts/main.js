64e382cd65960a64c438e217d5dbe6e4564acf7c
Wili Saputra <wilisaputra218@gmail.com>
2019-11-26 19:32:26 +0700

    Buat button single player dan multiplayer

---
+++ src/scripts/main.js
@@ -0,0 +1,14 @@
+
+
+
+function sp(){
+    document.getElementById('introPage').style.display = "none";
+    document.getElementById('multiPage').style.display = "none";
+    document.getElementById('singlePage').style.display = "block";
+}
+
+function mp(){
+    document.getElementById('introPage').style.display = "none";
+    document.getElementById('singlePage').style.display = "none";
+    document.getElementById('multiPage').style.display = "block";
+}
