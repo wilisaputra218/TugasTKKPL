8ac4466a85b168a1fcb54c8feb358466a91d5fbf
Ricky <rickylim43@gmail.com>
2019-11-30 00:13:57 +0700

    Mengatur Jumlah Karakter

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -15,7 +15,10 @@ function mp(){
     document.getElementById('multiPage').style.display = "block";
 }
 
-
+function countChars(countfrom,displayto) {
+    var len = document.getElementById(countfrom).value.length;
+    document.getElementById(displayto).innerHTML = len;
+}
 
 function readText(){
     word = document.getElementById('input').value;
